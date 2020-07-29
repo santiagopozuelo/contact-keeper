b1ffdf01928e5bff060dce063b1e12dc603a71fa
Santiago Pozuelo <santiagopozuelo@hotmail.com>
2020-07-29 09:51:34 -0600

    enhancements validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 //@route    POST api/users
 //@desc     Register user
 //@access   Public
 
-router.post('/', (req,res)=> {
-    res.send('registers user')
+router.post('/', [
+    check('name', 'name is required').not().isEmpty(),
+    check('email','please include valid email').isEmail(),
+    check('password', 'please enter password with 6 or more characters').isLength({ min: 6})
+], (req,res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+    res.send('passed');
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express()
+
+//connect database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }))
+
 
 app.get('/', (req,res) => res.json({msg: 'helloworld'}));
 
